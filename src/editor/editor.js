d2b2f9447ffacfec8c7f7167487ccfa52f2d4f77
Andydenike <denikeandrew@gmail.com>
2019-11-05 21:51:01 -0800

    Added ReactQuill / added selecting notes functionalities

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -8,10 +8,46 @@ import styles from './styles';
 class EditorComponent extends React.Component {
     constructor() {
         super();
+        this.state = {
+            text: '',
+            title: '',
+            id: ''
+        };
     }
+
+    componentDidMount = () => {
+        this.setState({
+            text: this.props.selectedNote.body,
+            title: this.props.selectedNote.title,
+            id: this.props.selectedNote.id
+        });
+    }
+
     render() {
-        return(<div>Hello from the editor</div>);
+
+        const { classes } = this.props;
+
+        return(
+            <div className={classes.editorContainer}>
+                <ReactQuill 
+                    //string of html == value 
+                    value={this.state.text} 
+                    onChange={this.updateBody}>
+                </ReactQuill>
+            
+            </div>
+        );
     }
+
+    updateBody = async (val) => {
+        await this.setState({ text: val });
+        this.update(); 
+    };
+
+    update = debounce(() => {
+        console.log('UPDATING DB');
+        //come back later
+    }, 1500);
 }
 // this.props.classes.classname
-export default withStyles(styles)(EditorComponent)
+export default withStyles(styles)(EditorComponent);
